ed092094123fd40866d06b8d81c8a3121c0d8771
khushi singhal <khushisinghal201120@gmail.com>
2024-05-19 20:15:24 +0530

    OperatIOns

---
+++ operations/arithmetic.js
@@ -0,0 +1,9 @@
+console.log("Operators in js")
+let a=78 ; 
+b=64 ;
+console.log("a+b = ",a+b)
+console.log("a-b = ",a-b)
+console.log("a*b = ",a*b)
+console.log("a/b = ",a/b)
+console.log("b/a = ",b/a)
+console.log("b-a = ",b-a)
