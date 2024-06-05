0c73c68298df3da5a189662d16fc61308c8c4234
khushi singhal <khushisinghal201120@gmail.com>
2024-06-05 23:23:39 +0530

    1step

---
+++ DATA type/data.js
@@ -0,0 +1,9 @@
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
---
+++ Object/obj.js
@@ -0,0 +1,11 @@
+let stu = {
+    "name": "Harry",//without spaces variable can be quoted or not 
+    "roll no": 5600,// if variable name contain spaces it contain " "
+    "is handsome": true
+}
+
+console.log(stu);
+stu.fees= "100crores";
+console.log(stu);
+stu.fees = "500crores";
+console.log(stu);
