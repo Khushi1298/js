505cfc67877bc7c0eab6c4212740923597a7c6e7
khushi singhal <khushisinghal201120@gmail.com>
2024-06-05 23:05:52 +0530

    VAR

---
+++ VARiables/var.js
@@ -0,0 +1,12 @@
+console.log("HELLO JS")
+let a = 10 ;
+let b = 30 ;
+var c = 40 ; //global var
+const d = 34 ; //constant var
+c=c+1
+console.log(a+b) ;
+console.log(a+c) ;
+console.log(d+1) ;
+console.log(c+10) ;
+console.log("HELLO JS")
+//in js mostly let variable is used 
