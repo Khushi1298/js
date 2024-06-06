63c8fdd7f3486c40d51f339dff5ee83c97d0c25a
khushi singhal <khushisinghal201120@gmail.com>
2024-06-06 23:04:15 +0530

    Changes

---
+++ operations/comparison.js
@@ -0,0 +1,15 @@
+//"3"==3 Compares the equality of two operators(==)
+//"5"===5 Compares both the value and type of the operand(===)
+// Illustration of (==) operator
+let val1 = 5;
+let val2 = '5';
+
+// Checking of operands
+console.log(val1 == 5);
+console.log(val2 == 5);        
+console.log(val1 == val1);
+console.log(val1===val2)
+
+// Check against null and boolean value
+console.log(0 == false);   
+console.log(0 == null);
