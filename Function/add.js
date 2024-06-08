140f140707b98dab5e07326707a617a25a798791
khushi singhal <khushisinghal201120@gmail.com>
2024-06-08 22:47:28 +0530

    Func

--- Function/add.js
+++ Function/add.js
@@ -1,8 +1,10 @@
-function sum(a,b)
+function sum(a,b,c=2)//c is default 
 {
-    return a+b ;
+    return a+b+c;
 }
  result = sum(4,5)
  ans = sum(8,2)
+ ad =sum(2,4,3)
  console.log("add1 :",result)
  console.log("add2 :",ans)
+ console.log("add3 :",ad)
