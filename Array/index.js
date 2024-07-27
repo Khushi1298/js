1884a03416f72d0592cc4bf0c8d330c3bbd4539e
khushi singhal <khushisinghal201120@gmail.com>
2024-07-27 21:16:07 +0530

    ARR..y

---
+++ Array/index.js
@@ -0,0 +1,18 @@
+let arr=[2,4,5,6,7] ;
+console.log(arr) ;
+console.log(arr.length) ;
+console.log(arr[0]) ;
+console.log(arr[1]) ;
+console.log(arr[2]) ;
+console.log(arr[3]) ;
+console.log(arr[4]) ;
+arr[0]=234 ;
+console.log(arr) ;
+console.log(arr.toString())
+console.log(arr.join(" and "))
+console.log(arr.pop(arr[4]))
+console.log(arr.pop(arr[3]))
+console.log(arr.push(8))
+console.log(arr) ;
+console.log(arr.push("khushi"))
+console.log(arr) ;
---
+++ Array/shift.js
@@ -0,0 +1,5 @@
+let a=[2,3,4,5]
+console.log(a)
+console.log(a.shift())//remove first element 
+console.log(a)
+console.log(a.unshift("k")) ;
