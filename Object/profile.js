322b4f16a9351198be81e85c423ee0be289bf8e1
khushi singhal <khushisinghal201120@gmail.com>
2024-09-23 12:10:25 +0530

    object

--- Object/profile.js
+++ Object/profile.js
@@ -4,9 +4,12 @@ let profile={
     bio:"Call me Singhal",
     post:10,
     following:200,
-    followers:130
+    followers:130,
+    isfollowing:true
 };
 console.log(profile)
 console.log(profile["post"])
 profile["followers"] =150
 console.log(profile["followers"])
+console.log(profile["isfollowing"])
+console.log(typeof profile["isfollowing"])
