57c3b60f68ba59c7ccd4a5b24c4d18c90830d660
khushi singhal <khushisinghal201120@gmail.com>
2024-09-23 11:22:02 +0530

    object

---
+++ Object/product.js
@@ -0,0 +1,12 @@
+let product ={
+    Item:"Pen" ,
+    Brand:"Doms",
+    Type:"Ball Pen",
+    Colour:"Black",
+    Price:20,
+    Offer:2,
+    Rating:5,
+};
+console.log(product)
+console.log(product["Brand"])
+console.log(product["Offer"])
