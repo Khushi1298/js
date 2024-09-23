88e69b651243f01bed87d7db621dd2edc29ffe9d
khushi singhal <khushisinghal201120@gmail.com>
2024-09-23 12:00:27 +0530

    object

---
+++ Object/profile.js
@@ -0,0 +1,12 @@
+let profile={
+    username:"khushi_singhal31",
+    name:"k_singhal",
+    bio:"Call me Singhal",
+    post:10,
+    following:200,
+    followers:130
+};
+console.log(profile)
+console.log(profile["post"])
+profile["followers"] =150
+console.log(profile["followers"])
